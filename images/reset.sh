4bc7dea9c642edd165c1e6b0fb58caa2d3ac42db
wention <xuyi.828@gmail.com>
2024-10-19 12:30:15 +0800

    update

---
+++ images/reset.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+BASEDIR=$(realpath $(dirname $0))
+
+rsync -u ~/Git/lede/bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.qcow2 $BASEDIR
+qemu-img create -f qcow2 -b $BASEDIR/openwrt-x86-64-generic-squashfs-combined-efi.qcow2 -F qcow2 $BASEDIR/openwrt.qcow2 
+
